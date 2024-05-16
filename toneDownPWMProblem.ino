2bfb846a1ceb502c865ad103acd0148eb08ad8c5
dadalee51 <dadalee51@gmail.com>
2024-05-17 00:38:46 +1000

    update

--- toneDownPWMProblem.ino
+++ toneDownPWMProblem.ino
@@ -1,22 +1,24 @@
 #include <Arduino.h>
-#include <Wire.h>
-#define FOR(I,N) for(int I=0;I<N;I++)
 void show_RGB(long val);
 void setup() {
-  pinMode(PIN_PB3, OUTPUT);
+  pinMode(PIN_PA3, OUTPUT);
   pinMode(PIN_PB4, OUTPUT);
   pinMode(PIN_PB5, OUTPUT);
-  PORTMUX_CTRLC |= PORTMUX_TCA00_bm | PORTMUX_TCA01_bm | PORTMUX_TCA02_bm;
-  show_RGB(0xFFFF00); 
+  show_RGB(0xFFFFFF); 
 }
+int data=0;
 void loop() {  
-    show_RGB(0xFFFF00); 
-    delay(500);
-    show_RGB(0xFFFFAA);
-    delay(500);
+    if(data>0xFE)data=0;
+    data+=20;
+    show_RGB(data<<16 | 0xFFFF); 
+    delay(10);
+    show_RGB(data<<8 | 0xFF00FF); 
+    delay(10);
+    show_RGB(data | 0xFFFF00); 
+    delay(10);
 }
 void show_RGB(long val){
-    analogWrite(PIN_PB3,(val>>16) & 0xFF);
+    analogWrite(PIN_PA3,(val>>16) & 0xFF);
     analogWrite(PIN_PB4,(val>>8)  & 0xFF);
     analogWrite(PIN_PB5,val       & 0xFF);
 }
